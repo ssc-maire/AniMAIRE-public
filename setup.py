ec1c7866edca6ab512ed6b5e4ddb39683e986330
Chris Davis <ChrisSWDavis@gmail.com>
2024-03-06 15:17:15 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -20,7 +20,10 @@ long_description = (this_directory / "README.md").read_text()
 setup(
     name='AniMAIRE',
     packages=find_packages(exclude='pytests'),
-    version='1.0.6',
+    package_data={"AniMAIRE":[
+                                "anisotropic_MAIRE_engine/data/*.csv"
+                                         ]},
+    version='1.0.7',
     description='Python library for running the anisotropic version of MAIRE+',
     long_description=long_description,
     long_description_content_type='text/markdown',
