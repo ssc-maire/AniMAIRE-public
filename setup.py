199a177d0e909aabdfb94917b7a1b694e4a7cd73
Chris Davis <ChrisSWDavis@gmail.com>
2024-03-15 21:10:38 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     package_data={"AniMAIRE":[
                                 "anisotropic_MAIRE_engine/data/*.csv"
                                          ]},
-    version='1.0.15',
+    version='1.1.0',
     description='Python library for running the anisotropic version of MAIRE+',
     long_description=long_description,
     long_description_content_type='text/markdown',
@@ -33,6 +33,7 @@ setup(
     url='https://github.com/ssc-maire/AniMAIRE-public',
     keywords='anisotropic MAIRE+ atmospheric ionizing radiation dose rates cosmic rays ground level enhancements GLEs protons alpha particles neutrons effective ambient equivalent aircraft aviation Earth solar system sun space magnetic field',
     install_requires=['numpy>=1.21.6',
+                      'wheel>=0.41.2',
                         'pandarallel>=1.6.3',
                         'pandas>=1.3.5',
                         'ParticleRigidityCalculationTools>=1.5.4',
@@ -41,7 +42,6 @@ setup(
                         'spacepy>=0.3.0',
                         'tqdm>=4.65.0',
                         'plotly>=5.9.0',
-                        'wheel>=0.41.2',
                         'numba>=0.57.1',
                         'metpy>=1.5.1',
                         'CosRayModifiedISO >= 1.2.3',
